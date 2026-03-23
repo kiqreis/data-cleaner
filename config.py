76011e3b605486446b968ccea26e8affc97f5614
Kaique Reis <kreis1265@gmail.com>
2026-03-23 19:18:38 -0300

    chore: add enums for statistical analysis metrics

---
+++ config.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class ImputationStrategy(str, Enum):
+    DROP = "drop"
+    MEAN = "mean"
+    MEDIAN = "median"
+    MODE = "mode"
+    ZERO = "zero"
+    FFILL = "ffill"
+    BFILL = "bfill"
+    VALUE = "value"
+
+
+class OutlierStrategy(str, Enum):
+    REMOVE = "remove"
+    CLIP = "clip"
+    FLAG = "flag"
+    NONE = "none"
+
+
+class OutlierMethod(str, Enum):
+    IQR = "iqr"
+    ZSCORE = "zscore"
+    MODIFIED = "modified_zscore"
