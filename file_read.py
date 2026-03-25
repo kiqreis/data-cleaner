a5327ee7b6adbcf325b9fdcfb78ff95c69673f97
Kaique Reis <kreis1265@gmail.com>
2026-03-24 23:21:26 -0300

    feat: add file reader and writer separated module

---
+++ file_read.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+def load_csv(path: str, **kwargs) -> pd.DataFrame:
+    return pd.read_csv(path, **kwargs)
+
+
+def load_excel(path: str, sheet_name: str | int = 0, **kwargs) -> pd.DataFrame:
+    return pd.read_excel(path, sheet_name=sheet_name, **kwargs)
+
+
+def save_csv(df: pd.DataFrame, path: str, **kwargs) -> None:
+    kwargs.setdefault("index", False)
+
+    df.to_csv(path, **kwargs)
+
+
+def save_excel(df: pd.DataFrame, path: str, sheet_name: str, **kwargs) -> None:
+    kwargs.setdefault("index", False)
+
+    df.to_excel(path, sheet_name=sheet_name, **kwargs)
